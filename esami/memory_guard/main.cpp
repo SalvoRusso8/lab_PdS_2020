05c9879aae117d42b71c7f7b4e0efb789a547d01
dualjk <46673945+dualjk@users.noreply.github.com>
2020-09-09 11:20:18 +0200

    Aggiornato con il codice del professore

--- esami/memory_guard/main.cpp
+++ esami/memory_guard/main.cpp
@@ -1,3 +1,36 @@
+/* Soluzione ufficiale del prof. Malnati: 
+ * #include <iostream>
+#include <thread>
+#include <mutex>
+class semaphore {
+    int count;
+    std::mutex m;
+    std::condition_variable cv;public:
+    explicit semaphore(int count): count(count) {}
+    void acquire() {
+        std::unique_lock ul(m);
+        cv.wait(ul,[this]() {return this->count>0;});
+        count--;
+    }
+    void release() {
+        std::unique_lock<std::mutex> ul(m);
+        count++;
+        cv.notify_one();
+    }
+};class raii_guard {
+    semaphore& s;
+public:
+    explicit raii_guard(semaphore& s): s(s) {
+        s.acquire();
+    }
+    ~raii_guard() {
+        s.release();
+    }
+};*/
+
+
+
+
 #include <iostream>
 #include <vector>
 #include <memory>
